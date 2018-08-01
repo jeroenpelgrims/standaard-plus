7eddfe350715b2b7c67c6ab6451a4daa3472977e
Jeroen Pelgrims <jeroen.pelgrims@gmail.com>
2018-08-01 10:40:39 +0200

    Initial commit in git

---
+++ index.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @namespace https://jeroenpelgrims.com
+// @name Standaard plus
+// @name:nl Standaard plus
+// @description Toggle Standaard premium article classes.
+// @description:nl Toggle Standaard premium article classes.
+// @version  1.0.4
+// @grant    none
+// @include http://www.standaard.be/*
+// @downloadURL https://greasyfork.org/scripts/370805-standaard-plus/code/Standaard%20plus.user.js
+// @updateURL https://greasyfork.org/scripts/370805-standaard-plus/code/Standaard%20plus.meta.js
+// ==/UserScript==
+
+function toggleClasses() {
+	const toHide = [
+		'#porous-paywall-content',
+		'#porous-paywall-shortened-content',
+		'.article__body > p',
+		'span.label_plus'
+	];
+
+	document
+		.querySelectorAll(toHide.join(','))
+		.forEach(element => element.classList.toggle('is-hidden'));
+	document.body.classList.remove('is-closed-article');
+	document.querySelectorAll('.article--plus').forEach(element => element.classList.toggle('article--plus'));
+}
+
+toggleClasses();
+
+console.log('1.0.4');
